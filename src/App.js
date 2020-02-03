3220c29d65a42523815dbf27eab761b9e9217f95
mhmpoulard <grincinqmhp@hotmail.com>
2020-02-02 22:32:39 -0500

    minor change

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ class App extends Component {
     const { searchTerm } = this.state;
     this.fetchSearchTopstories(searchTerm);
   }
+  
 
 onSearchSubmit(event) {
   const { searchTerm }= this.state;
